682e90b81cf1355881a84d1d8409064f86aed713
Belen Ibanez <belenibanezm@gmail.com>
2025-11-06 14:34:25 +0000

    build files

---
+++ out/_next/static/uOntKW4fwEXwbDqn6NjWI/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/5ab8c7e778f1fc2e.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
